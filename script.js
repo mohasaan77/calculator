f2e271083f22e380e3b243e27ced9e09e4e75b07
Hasaan <55707828+mohasaan77@users.noreply.github.com>
2026-03-27 20:45:14 +0500

    Improved UI and added full functionality
    Improved the UI inconsistencies and minor bugs. Added
    functionality to all the buttons, operations etc.

--- script.js
+++ script.js
@@ -1,57 +1,126 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-// const divide = (a, b) => (b === 0 ? NaN : a / b);
-const divide = (a, b) => a / b;
+const divide = (a, b) => (b === 0 ? NaN : a / b);
 
 
-function operate(num1, num2, operator) {
-  switch(operator) {
+function operate(a, b, op) {
+  switch(op) {
     case '+':
-      return add(num1, num2);
+      return add(a, b);
       break;
     case '-':
-      return subtract(num1, num2);
+      return subtract(a, b);
       break;
     case '×':
-      return multiply(num1, num2);
+      return multiply(a, b);
       break;
     case '÷':
-      return divide(num1, num2);
+      return divide(a, b);
       break;
   }
 }
 
 
-let num1 = null;
-let num2 = null;
-let op = null;
+function calculate() {
+  num2 = Number(currentInput);
+  const result = operate(num1, num2, op);
+
+  if (result.toString().length >= 8) {
+    display.textContent = formatNumber(result).toPrecision(3);    
+  } else {
+    display.textContent = formatNumber(result);    
+  }
+
+  num1 = null;
+  num2 = null;
+  op = null;
+  currentInput = result.toString();
+}
+
+
+function formatNumber(num, digits = 2) {
+  return Number.isInteger(num) ? num : parseFloat(num.toFixed(digits));
+}
 
 
 const numbers = document.querySelectorAll(".number-btn");
 const operators = document.querySelectorAll(".operator-btn");
 const equalBtn = document.querySelector("#equal-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const backspaceBtn = document.querySelector("#backspace-btn");
+const percentageBtn = document.querySelector("#percentage-btn");
+const negationBtn = document.querySelector("#negation-btn");
 const display = document.querySelector("#display-content");
 
 
-numbers.forEach(number => {
-  number.addEventListener("click", () => {
-    display.textContent += number.value;
+let num1 = null;
+let num2 = null;
+let op = null;
+let currentInput = "";
+display.textContent = "0";
+
+
+numbers.forEach(button => {
+  button.addEventListener("click", () => {
+    if (display.textContent === "0" && button.value !== ".") display.textContent = "";
+    if (button.value === "." && currentInput.includes(".")) return;
+    if (display.textContent.length === 8 && op !== null) return;
+
+    currentInput += button.value;
+    display.textContent = currentInput;
   });
 });
 
 
-operators.forEach(operator => {
-  operator.addEventListener("click", () => {
-    num1 = Number(display.textContent);
-    display.textContent += operator.textContent;
-    op = operator.value;
+operators.forEach(button => {
+  button.addEventListener("click", () => {
+    if (currentInput === "") return;
+    if (op !== null && num2 === null) calculate();
+    if (op !== null && num2 !== null) return;
+
+    num1 = Number(currentInput);
+    op = button.value;
+    display.textContent += button.value;
+    currentInput = "";
   });
 });
 
-equalBtn.addEventListener("click", () => {
-  let fullDisplay = display.textContent;
-  num2 = Number(fullDisplay.split(op)[1]);
-  let result = operate(num1, num2, op);
-  display.textContent = result.toPrecision(3);
+
+equalBtn.addEventListener("click", () => calculate());
+
+
+clearBtn.addEventListener("click", () => {
+  display.textContent = "0";
+  num1 = null;
+  num2 = null;
+  op = null;
+  currentInput = "";
+});
+
+
+backspaceBtn.addEventListener("click", () => {
+  if (currentInput !== "") {
+    currentInput = currentInput.slice(0, -1);
+    display.textContent = currentInput || "0";
+    return;
+  }
+
+  if (op !== null) {
+    op = null;
+    display.textContent = display.textContent.slice(0, -1);
+    currentInput = display.textContent;
+    return;
+  }
+});
+
+
+negationBtn.addEventListener("click", () => {
+  if (currentInput.includes("-")) {
+    currentInput = currentInput.slice(1);
+    display.textContent = currentInput;
+  } else {
+    currentInput = "-" + currentInput;
+    display.textContent = currentInput;
+  }
 });
